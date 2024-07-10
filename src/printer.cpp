db578b84b1648aa7e23664a73d401b6010700267
adriankraw <adriankraw1@googlemail.com>
2024-07-10 12:11:05 +0200

    added printer.cpp

---
+++ src/printer.cpp
@@ -0,0 +1,60 @@
+#include <iomanip>
+#include <iostream>
+
+#include "render.cpp"
+#include "Character.cpp"
+
+class printer{
+public:
+	printer();
+	printer(printer &&) = default;
+	printer(const printer &) = default;
+	printer &operator=(printer &&) = default;
+	printer &operator=(const printer &) = default;
+	~printer();
+
+	render *ren;
+
+	void header();
+	void timer();
+	void characterStats(Character*);
+	void flush();
+
+private:
+};
+
+printer::printer() {
+	ren = new render();
+}
+
+printer::~printer() {
+	delete ren;
+}
+
+void printer::header() {
+
+	std::cout << std::left << "PomoRPG: "<< std::endl;
+	std::cout << std::setw(80) << std::setfill('_') << '_' << std::endl << std::endl;
+}
+void printer::timer(){
+	std::cout << "\033[1m";
+	for(int i(0); i < (ren->resultpointer)->size(); ++i)
+	{
+		std::cout << (*ren->resultpointer)[i] << std::endl;
+	}
+	std::cout << "\033[0m";
+
+	std::cout << std::setw(80) << std::setfill('_') << '_' << std::endl << std::endl;
+}
+void printer::characterStats(Character* character){
+	std::cout << "RPG:" << std::endl << std::endl;
+
+	std::cout << "Name \t" << character->Name() << std::endl;
+	std::cout << "LVL \t" << character->Lvl() << std::endl;
+	std::cout << "Exp \t" << character->Exp() << "/" << character->GetNextLevelExp() << std::endl;
+
+	std::cout << std::setw(80) << std::setfill('_') << '_' << std::endl << std::endl;
+}
+void printer::flush(){
+	std::cout << "\033[1J \033[1H" << std::flush;
+}
